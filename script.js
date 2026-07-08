4e4016300a7e2ea3016a72b4f441f92c57ced3fb
Vinicius Campos | VC Marketplace <contato@vcmarketplace.com.br>
2026-07-08 15:44:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,16 @@ const observer = new IntersectionObserver(entries => {
 
 });
 
+window.addEventListener("scroll", () => {
+
+    const header = document.querySelector(".header");
+
+    header.classList.toggle("scrolled", window.scrollY > 50);
+
+});
+
 document
 .querySelectorAll('.card, .hero-card')
 .forEach(el => observer.observe(el));
+
+
